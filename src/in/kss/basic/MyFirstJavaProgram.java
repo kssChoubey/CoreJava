e42ea4a1c7016665a3be4be25cf3295b55732181
kssChoubey <krishna0choubey@gmail.com>
2024-12-21 17:36:09 +0530

    testing

--- src/in/kss/basic/MyFirstJavaProgram.java
+++ src/in/kss/basic/MyFirstJavaProgram.java
@@ -3,5 +3,6 @@ package in.kss.basic;
 public class MyFirstJavaProgram {
     public static void main(String[] args) {
         System.out.println("Hello world! modified");
+        System.out.println("testing");
     }
 }
