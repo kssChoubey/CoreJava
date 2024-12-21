a49fde66a11c8752f0b336268e24eeac243373b6
kssChoubey <krishna0choubey@gmail.com>
2024-12-21 17:39:31 +0530

    configured with eclipse

--- src/in/kss/basic/MyFirstJavaProgram.java
+++ src/in/kss/basic/MyFirstJavaProgram.java
@@ -3,6 +3,6 @@ package in.kss.basic;
 public class MyFirstJavaProgram {
     public static void main(String[] args) {
         System.out.println("Hello world! modified");
-        System.out.println("testing");
+        System.out.println("configured with Eclipse");
     }
 }
